9f4464080a992ffb52ec63efec78cd2dfda7b855
elpe2005 <Epedersen2005@gmail.com>
2023-11-16 13:56:01 +0100

    meny klar animation kanske

---
+++ Assets/Playermove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+            public float speed = 1f;
+
+    void Update()
+    {
+
+        if(Input.GetKey(KeyCode.S)){
+            transform.position += new Vector3(0,0,-speed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.W)){
+            transform.position += new Vector3(0,0,speed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.D)){
+            transform.position += new Vector3(speed * Time.deltaTime,0,0);
+        }
+        if(Input.GetKey(KeyCode.A)){
+            transform.position += new Vector3(-speed * Time.deltaTime,0,0);  
+
+        }
+    }
+}
